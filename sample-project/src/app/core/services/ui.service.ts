fe3a54e4d84b307dce8f3fc56f9f3db0923ad272
Chamith95 <chamithjay@gmail.com>
2021-09-02 14:22:08 +0530

    files added

---
+++ sample-project/src/app/core/services/ui.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiService {
+
+  constructor(
+    private snackbar: MatSnackBar
+  ) { }
+
+ 
+}
